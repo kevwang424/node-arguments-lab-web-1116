f85031f6259cd92b6cf00e2f07584cc5e8b822d3
Kevin Wang <kevinwang@Kevins-Air.home>
2017-03-15 19:49:18 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,21 @@
 // code solution here
+var args = process.argv.splice(2)
+
+var lengthOfPasswords = args[0]
+var numOfPasswords = args[1]
+
+function generatePassword(len) {
+    var length = len
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    console.log(retVal);
+}
+
+var count = 1
+while (count <= numOfPasswords){
+  generatePassword(lengthOfPasswords)
+  count++
+}
